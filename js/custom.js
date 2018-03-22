c756e4f100cc8db7c405aa0f5a788a6492c356ab
unknown <C:\Users\Sergey\AppData\Roaming\The Bat!>
2018-03-22 21:04:55 +0300

    proxy  change

--- js/custom.js
+++ js/custom.js
@@ -184,7 +184,8 @@ $(document).ready(function() {
     var customAPI, url;
     // var API = 'https://cdn.pamyat-naroda.ru/ind2/';
     // var API = 'https://cdn.pamyatnaroda.mil.ru/ind/';
-    var API = 'https://python-flask-test-1153.appspot.com/obd/';
+    // var API = 'https://python-flask-test-1153.appspot.com/obd/';
+    var API = 'http://flask-pamyat-naroda.1d35.starter-us-east-1.openshiftapps.com/obd/';
     var imagesCDN = 'https://cdn.pamyat-naroda.ru/imageload/';
 
     $('#apiURL').val('');
